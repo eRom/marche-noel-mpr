892f3de90ce600ec73e08345bb4a8a0efee593d9
eRom <romain.ecarnot@gmail.com>
2025-10-29 20:05:50 +0100

    feat: Add new galerie entry to sitemap
    - Included a new entry for the galerie page in the sitemap with daily change frequency and updated last modified date.
    - Maintained existing structure and priority settings for consistency.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,6 +17,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/galerie`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/a-propos`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
